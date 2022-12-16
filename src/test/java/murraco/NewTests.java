c000f47f4203aad02192d28e9fd5efe1c83db7e9
Theo <theo.le.wiik@gmail.com>
2022-12-16 14:32:56 +0100

    Rename better test suite

--- src/test/java/murraco/NewTests.java
+++ src/test/java/murraco/NewTests.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
-public final class BetterTestSuite {
+public final class NewTests {
 
   @Test
   public void shouldThrowIllegalArgumentIfArrayIsNull() {
